fffdd7850ec4a71e9f5020a32154c236dca4b06a
rk8672 <rky8672@gmail.com>
2025-06-26 12:26:22 +0530

    prising change

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import FeatureSection from "./Feature_Section";
 import Footer from "./Footer_section";
 import HeroSection from "./Hero_Section";
 import HowWorks from "./How_Woerks_Section";
-import PricingSection from "./Pricing_section";
+import PricingSection from "./Pricing_Section";
 
 const Home=()=>{
 
