8cad05fc6009a2a673c7fff7f3f31c8be86c7b7d
rk8672 <rky8672@gmail.com>
2025-07-04 10:41:19 +0530

    webhook  routing

--- backend/index.js
+++ backend/index.js
@@ -9,20 +9,22 @@ app.use(cors());
 app.use(express.json()); // Handles application/json
 app.use(express.urlencoded({ extended: true })); // Handles form data
 
-const whatsappWebhook=require('./webhook/whatsappWebhook');
 const dataConnection = require("./config/db.js");
 dataConnection();
 
+const whatsappWebhook=require('./webhook/whatsappWebhook');
 
+app.use('/webhook',whatsappWebhook)
+
+//Index Route
+const main = require("./routes/index.js");
+app.use('/api', main);
 
 app.get('/', (req, res) => {
     res.send("Hello from Backend Team - Radha Krishna Singh");
 })
 
-//Index Route
-const main = require("./routes/index.js");
-app.use('/api', main);
-app.use('/webhook',whatsappWebhook)
+
 
 const port = process.env.PORT || 10000; 
 
