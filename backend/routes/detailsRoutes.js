5c661625a2fa9280eacfee75353bfbc0430101b5
rk8672 <rky8672@gmail.com>
2025-09-15 20:49:45 +0530

    backend

--- backend/routes/detailsRoutes.js
+++ backend/routes/detailsRoutes.js
@@ -3,7 +3,7 @@ import { getWhatsappDetails } from "../controllers/detailsController.js";
 
 const router = express.Router();
 
-// POST /api/whatsapp/detail
+
 router.post("/", getWhatsappDetails);
 
 export default router;
